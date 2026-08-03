abe6d9d76fb20007351fb0f6c3c3830afd95a8e7
melonmann12 <hongquanvjp@gmail.com>
2026-08-04 00:37:03 +0700

    01td

--- components/analytics/MetaPixel.tsx
+++ components/analytics/MetaPixel.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { usePathname, useSearchParams } from 'next/navigation'
 import Script from 'next/script'
 import { PIXEL_ID, pageview } from '@/lib/analytics/metaPixel'
@@ -8,10 +8,16 @@ import { PIXEL_ID, pageview } from '@/lib/analytics/metaPixel'
 export default function MetaPixel() {
   const pathname = usePathname()
   const searchParams = useSearchParams()
+  const lastTrackedUrl = useRef<string | null>(null)
 
   useEffect(() => {
     // Only fire if the script has actually loaded
     if (typeof window !== 'undefined' && window.fbq) {
+      const url = pathname + (searchParams?.toString() ? `?${searchParams.toString()}` : '')
+      
+      if (lastTrackedUrl.current === url) return
+      
+      lastTrackedUrl.current = url
       pageview()
     }
   }, [pathname, searchParams])
@@ -33,7 +39,6 @@ export default function MetaPixel() {
             'https://connect.facebook.net/en_US/fbevents.js');
             
             fbq('init', '${PIXEL_ID}');
-            fbq('track', 'PageView');
           `,
         }}
       />
